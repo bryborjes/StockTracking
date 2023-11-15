c9111e3c9fe3855576f0d2373909c900e6c7e343
bryborjes <114537326+bryborjes@users.noreply.github.com>
2023-11-14 20:10:33 -0600

    Add files via upload

---
+++ StockTracking_Objecs-BryBorj/StockTracking_Objecs-BryBorj/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("StockTracking")]
+[assembly: AssemblyDescription("Elaborado con VS 2010. Maneger: \n Francisco Javier Brachi Lopez. Alumno: Bryan Albino Borjes")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CBTis N°254")]
+[assembly: AssemblyProduct("Stock-Tracking")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0c118804-413f-4bfb-95cf-a6400427817a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("es-MX")]
